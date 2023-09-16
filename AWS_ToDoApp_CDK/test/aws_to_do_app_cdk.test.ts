7af7c45606ef29c625d62ed6807aa4c014b1306d
angelDariaux <angeldariaux@gmail.com>
2023-09-15 22:40:05 -0600

    started the CDK app

---
+++ AWS_ToDoApp_CDK/test/aws_to_do_app_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsToDoAppCdk from '../lib/aws_to_do_app_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws_to_do_app_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsToDoAppCdk.AwsToDoAppCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
