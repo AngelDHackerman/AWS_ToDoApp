fb79c66ad6158506f00554d611b0de1362b5c1ee
angelDariaux <angeldariaux@gmail.com>
2023-09-16 22:15:20 -0600

    finished code of create-task.ts function

--- AWS_ToDoApp_CDK/lambda/create-task.ts
+++ AWS_ToDoApp_CDK/lambda/create-task.ts
@@ -1,17 +1,35 @@
-import * as AWS from 'aws-sdk';
-const dynamo = new AWS.DynamoDB.DocumentClient();  // DocumentClient es una interfaz de alto nivel en el SDK de AWS que proporciona métodos para realizar operaciones en DynamoDB sin tener que trabajar directamente con los tipos de datos nativos de DynamoDB. Es más fácil y conveniente para trabajar con datos en formato JSON.
-
-export.handler = async (event) => { 
-  const body = JSON.parse(event.body);
-  const { taskId, dueDate } = body  // parametros que debe recibir la base de datos de dynamo
-
-  const params = { 
-    TableName: process.env.TABLE_NAME, // El nombre de la tabla se pasa como variable de entorno. 
-    Item: { 
-      taskId,
-      dueDate,
-      expiryDate: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 7  // TTL de 7 dias
-
-    }
-  };
+interface LambdaEvent { 
+  body: string;
 }
+
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event: LambdaEvent) => {
+    const body = JSON.parse(event.body);
+    const { taskId, dueDate } = body;
+
+    const params = {
+        TableName: process.env.TABLE_NAME,  // El nombre de la tabla se pasa como variable de entorno
+        Item: {
+            taskId,
+            dueDate,
+            expiryDate: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 7  // TTL de 7 días
+        }
+    };
+
+    try {
+        await dynamo.put(params).promise();
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Task added successfully' }),
+        };
+    } catch (error) {
+      // Hacemos un type assertion aqui
+      const errMsg = (error as Error).message
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: errMsg }),
+        };
+    }
+};
