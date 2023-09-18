501e63ba49ef04dc96b9cf1c1a90f344a95582c5
angelDariaux <angeldariaux@gmail.com>
2023-09-17 22:25:51 -0600

    finished to prepare code for the lambda funcion create-task

--- AWS_ToDoApp_CDK/lib/aws_to_do_app_cdk-stack.ts
+++ AWS_ToDoApp_CDK/lib/aws_to_do_app_cdk-stack.ts
@@ -1,11 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import { DynamoDBTable } from './dynamodb-table';  
+import * as path from 'path'; 
+import { LambdaApplication } from 'aws-cdk-lib/aws-codedeploy';
 
 export class AwsToDoAppCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new DynamoDBTable(this, 'DynamoDBTableConstruct')
+    const dynamoTable = new DynamoDBTable(this, 'DynamoDBTableConstruct');
+
+    const createTaskFunction = new lambda.Function(this, 'CreateTaskFunction', {  // está indicando que el código fuente para la función AWS Lambda CreateTaskFunction se encuentra en un directorio llamado lambda que está al mismo nivel que el directorio del archivo que contiene tu clase AwsToDoAppCdkStack. 
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'create-task.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda')),
+      environment: { 
+        TABLE_NAME: dynamoTable.table.tableName  // con esta linea estamos configurando una variable de entorno llamada TABLE_NAME para la funcion lambda. 
+      }
+    });
+
+    // Otorgar permisos a la funcion Lambda para escribir en la tabla DynamoDB
+    dynamoTable.table.grantWriteData(createTaskFunction)
   }
 }
