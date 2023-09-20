89cdfe47a6e9a5ba9b8bac136dd6f67ef2602e57
angel hernandez <angel.hernandez_santiago@telus.com>
2023-09-20 16:53:47 -0600

    basic code for update-task function created

--- AWS_ToDoApp_CDK/lambda/update-task.ts
+++ AWS_ToDoApp_CDK/lambda/update-task.ts
@@ -1,3 +1,58 @@
 
 // TODO: Refactor for dependency injection, now wer are crearting an instance of the DynamoDb table for each lambda function
 
+import * as AWS from 'aws-sdk';
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+interface LambdaEvent { 
+  pathParameters?: { 
+    taskId?: string,
+  };
+  body: string
+}
+
+exports.handler = async (event: LambdaEvent) => { 
+  const taskId = event.pathParameters?.taskId;
+  const body = JSON.parse(event.body);
+  const { status, dueDate } = body;
+
+  // todo: agregar una validacion para cuando se recibe un ID, pero no esta en la base de datos.
+  if (!taskId) { 
+    return { 
+      statusCode: 400,
+      body: JSON.stringify( { error: 'Task ID is required'})
+    };
+  }
+
+  const params: AWS.DynamoDB.DocumentClient.UpdateItemInput = { 
+    TableName: process.env.TABLE_NAME!,
+    Key: { taskId }, 
+    // Esta es una cadena que define las acciones que se realizarán en los atributos del ítem. En este caso, estás estableciendo (o actualizando) los valores de dos atributos: uno que se refiere con #statusAttribute y otro que se llama directamente dueDate.
+    // #statusAttribute: Es un nombre de atributo reservado. En DynamoDB, si estás usando palabras reservadas o quieres evitar problemas con nombres de atributos que podrían ser palabras reservadas en el futuro, puedes usar esta notación. :statusValue y :dueDateValue: Son los valores que quieres establecer para esos atributos. Estos valores se definen en ExpressionAttributeValues.
+    UpdateExpression: "set #statusAttribute = :statusValue, dueDate = :dueDateValue",
+    // Aquí estás definiendo lo que realmente significa #statusAttribute. En este caso, estás diciendo que cuando uses #statusAttribute en tu UpdateExpression, en realidad te estás refiriendo al atributo status en DynamoDB.
+    ExpressionAttributeNames: { 
+      '#statusAttribute': 'status'
+    },
+    // Aquí estás definiendo los valores para los marcadores de posición que usaste en UpdateExpression. Estás diciendo que :statusValue debe reemplazarse por el valor de status y :dueDateValue debe reemplazarse por el valor de dueDate que obtuviste del cuerpo del evento.
+    ExpressionAttributeValues: { 
+      ':statusValue': status,
+      ':dueDateValue': dueDate,
+    },
+    ReturnValues: "UPDATED_NEW"
+  };
+
+  try {
+    const result = await dynamo.update(params).promise();
+    return { 
+      statusCode: 200,
+      body: JSON.stringify( { message: 'Task updated successfully', updatedAttributes: result.Attributes})
+    }
+  } catch (error) {
+    const errMsg = (error as Error).message;
+    return { 
+      statusCode: 500,
+      body: JSON.stringify({ error: errMsg }),
+    }
+  }
+}
