37934e24c946bd8cfbafb3ac9c5aad0b1b0d76d5
angelDariaux <angeldariaux@gmail.com>
2023-09-23 21:54:22 -0600

    changed table environment for the new table with the new structure and changed the permissions for the lambda functions in the stacks file

--- AWS_ToDoApp_CDK/lib/aws_to_do_app_cdk-stack.ts
+++ AWS_ToDoApp_CDK/lib/aws_to_do_app_cdk-stack.ts
@@ -12,7 +12,7 @@ export class AwsToDoAppCdkStack extends cdk.Stack {
     super(scope, id, props);
 
     const dynamoTable = new DynamoDBTable(this, 'DynamoDBTableConstruct');
-    const dynamoTableV2 = new DynamoDBTable(this, 'DynamoDBTableConstructV2');  
+    const dynamoTableV2 = new DynamoDBTable(this, 'DynamoDBTableConstructV2');  // <- nuevo construct
 
     // Creando la funcion 'create-task'
     const createTaskFunction = new lambda.Function(this, 'CreateTaskFunction', {  // está indicando que el código fuente para la función AWS Lambda CreateTaskFunction se encuentra en un directorio llamado lambda que está al mismo nivel que el directorio del archivo que contiene tu clase AwsToDoAppCdkStack. 
@@ -20,13 +20,13 @@ export class AwsToDoAppCdkStack extends cdk.Stack {
       handler: 'create-task.handler',
       code: lambda.Code.fromAsset(path.join(__dirname, '../lambda')),
       environment: { 
-        TABLE_NAME: dynamoTable.table.tableName  // con esta linea estamos configurando una variable de entorno llamada TABLE_NAME para la funcion lambda. 
+        TABLE_NAME: dynamoTableV2.table.tableName   // con esta linea estamos configurando una variable de entorno llamada TABLE_NAME para la funcion lambda. 
       }
     });
 
     // Otorgar permisos a la funcion Lambda para escribir en la tabla DynamoDB
-    dynamoTable.table.grantWriteData(createTaskFunction);
-    dynamoTable.table.grantReadData(createTaskFunction);
+    dynamoTableV2.table.grantWriteData(createTaskFunction);
+    dynamoTableV2.table.grantReadData(createTaskFunction);
 
     // Definir la nueva funcion Lambda 'get-task-by-status' (que busca porque query las task segun su status)
     const getTaskFunction = new lambda.Function(this, 'GetTaskByStatusFunction', { 
@@ -34,12 +34,12 @@ export class AwsToDoAppCdkStack extends cdk.Stack {
       handler: 'get-task-by-status.handler',
       code: lambda.Code.fromAsset(path.join(__dirname, '../lambda')),
       environment: { 
-        TABLE_NAME: dynamoTable.table.tableName
+        TABLE_NAME: dynamoTableV2.table.tableName 
       },
     });
 
     // Otorgar permisos a la funcion Lambda para leer de la tabla DynamoDB
-    dynamoTable.table.grantReadData(getTaskFunction);
+    dynamoTableV2.table.grantReadData(getTaskFunction);
 
     // Definir la funcion 'get-task-by-id' que regresara las tareas segun su ID. 
     const getTaskByIdFunction = new lambda.Function(this, 'GetTaskByIdFunction', {
@@ -47,12 +47,12 @@ export class AwsToDoAppCdkStack extends cdk.Stack {
       handler: 'get-task-by-id.handler',
       code: lambda.Code.fromAsset(path.join(__dirname, '../lambda')),
       environment: { 
-        TABLE_NAME: dynamoTable.table.tableName
+        TABLE_NAME: dynamoTableV2.table.tableName 
       },
     });
 
     // Otorgar permisos a la funcion getTaskById
-    dynamoTable.table.grantReadData(getTaskByIdFunction);
+    dynamoTableV2.table.grantReadData(getTaskByIdFunction);
 
     // Definir la funcion 'update-task' que actualizara las tareas
     const updateTaskByIdFunction = new lambda.Function(this, 'updateTaskByIdFunction', {
@@ -60,12 +60,12 @@ export class AwsToDoAppCdkStack extends cdk.Stack {
       handler: 'update-task.handler',
       code: lambda.Code.fromAsset(path.join(__dirname, '../lambda')),
       environment: {
-        TABLE_NAME: dynamoTable.table.tableName
+        TABLE_NAME: dynamoTableV2.table.tableName 
       },
     });
 
     // otorgar permisos a la funcion updateTaskById
-    dynamoTable.table.grantWriteData(updateTaskByIdFunction)
-    dynamoTable.table.grantReadData(updateTaskByIdFunction);
+    dynamoTableV2.table.grantWriteData(updateTaskByIdFunction)
+    dynamoTableV2.table.grantReadData(updateTaskByIdFunction);
   }
 }
